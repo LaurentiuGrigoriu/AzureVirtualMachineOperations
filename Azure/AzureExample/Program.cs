4b680e459dc6654f618cb4009581c964296992a0
Laurentiu Grigoriu <laurentiu.gr@gmail.com>
2022-04-14 19:33:17 +0300

    Azure Virtual Machine API

---
+++ Azure/AzureExample/Program.cs
@@ -0,0 +1,88 @@
+﻿using AzureOps;
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Azure.Management.Network.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureExample
+{
+    class Program
+    {
+        // VM variables
+        static public class vmDetails
+        {
+            static public string resourceGroupName = "RG-FluentResourceGroup";
+            static public Region location = Region.USCentral;
+            static public string vmName = "VMFluent";
+            static public string vNetName = "VNET-Fluent";
+            static public string vNetAddress = "172.16.0.0/16";
+            static public string subnetName = "Subnet-Fluent";
+            static public string subnetAddress = "172.16.0.0/24";
+            static public string nicName = "NIC-Fluent";
+            static public string adminUser = "azureadminuser";
+            static public string adminPassword = "Pas$m0rd$123";
+            static public string publicIPName = "publicIP-Fluent";
+            static public string nsgName = "NSG-Fluent";
+        }
+
+        static void CreateVM(AzureAccount azure)
+        {
+            Console.WriteLine($"Creating resource group {vmDetails.resourceGroupName} ...");
+            azure.CreateResourceGroup(vmDetails.resourceGroupName, vmDetails.location);
+
+            //Every virtual machine needs to be connected to a virtual network.
+            Console.WriteLine($"Creating virtual network {vmDetails.vNetName} ...");
+            INetwork network = azure.CreateVirtualNetwork(vmDetails.vNetName, vmDetails.resourceGroupName, vmDetails.location, vmDetails.vNetAddress, vmDetails.subnetAddress, vmDetails.subnetName);
+
+            //You need a public IP to be able to connect to the VM from the Internet
+            Console.WriteLine($"Creating public IP {vmDetails.publicIPName} ...");
+            IPublicIPAddress publicIP = azure.CreatePublicIp(vmDetails.publicIPName, vmDetails.resourceGroupName, vmDetails.location);
+
+            //You need a network security group for controlling the access to the VM
+            Console.WriteLine($"Creating Network Security Group {vmDetails.nsgName} ...");
+            INetworkSecurityGroup nsg = azure.CreatNetworkSecurityGroup(vmDetails.resourceGroupName, vmDetails.nsgName, vmDetails.location);
+
+            //You need a security rule for allowing the remote TCP connection over the Internet
+            Console.WriteLine($"Creating a Security Rule for allowing the remote");
+            azure.AllowInternetConnection(nsg, 3389, SecurityRuleProtocol.Tcp);
+
+            Console.WriteLine($"Creating network interface {vmDetails.nicName} ...");
+            INetworkInterface nic = azure.CreateNetworkInterface(vmDetails.nicName, vmDetails.resourceGroupName, vmDetails.location, network, vmDetails.subnetName, publicIP, nsg);
+
+            Console.WriteLine($"Creating virtual machine {vmDetails.vmName} ...");
+            azure.CreateVM(vmDetails.vmName, vmDetails.resourceGroupName, vmDetails.location, nic, vmDetails.adminUser, vmDetails.adminPassword);
+        }
+
+        static void Main(string[] args)
+        {
+            AzureAccount azure = new AzureAccount("../../azure-configuration.json");
+
+            // create VM in azure AzureAccount, according to vmDetails values
+            CreateVM(azure);
+
+            AzureVM vm = new AzureVM(azure._azure, vmDetails.vmName);
+
+            // check the creation of 
+            PowerState state = vm.GetState();
+            if (state == PowerState.Running)
+                Console.WriteLine("Successfully created a new VM: {0}!", vmDetails.vmName);
+            
+            vm.Stop();
+
+            state = vm.GetState();
+            if (state == PowerState.Stopping || state == PowerState.Stopped)
+                Console.WriteLine("Successfully stopped the VM: {0}!", vmDetails.vmName);
+
+            azure.DeleteVM(vmDetails.resourceGroupName, vmDetails.vmName);
+            Console.WriteLine("VM {0} deleted.", vmDetails.vmName);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Azure/AzureVMOps/AzureVM.cs
@@ -0,0 +1,234 @@
+﻿using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.Network.Fluent;
+using Microsoft.Azure.Management.Network.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.Azure.Management.Fluent.Azure;
+
+namespace AzureOps
+{
+    public class AzureAccount
+    {
+        public IAzure _azure { get;  }
+
+        public AzureAccount(IAzure azure)
+        {
+            _azure = azure;
+        }
+
+        public AzureAccount(AzureCredentials credentials)
+        {
+            _azure = Azure.Authenticate(credentials).WithDefaultSubscription();
+            //_azure = Azure.Authenticate(credentials).WithSubscription(/*string */subscriptionId);
+        }
+
+        public AzureAccount(string authFile)
+        {
+            AzureCredentials credentials = SdkContext.AzureCredentialsFactory.FromFile(authFile);
+            _azure = Azure.Authenticate(credentials).WithDefaultSubscription();            
+        }
+
+        // create a resource group
+        public IResource CreateResourceGroup(string resourceGroupName, Region location)
+        {
+            IResource resourceGroup = _azure.ResourceGroups.Define(resourceGroupName)
+                .WithRegion(location)
+                .Create();
+
+            return resourceGroup;
+        }
+
+        // Delete a resource group by name
+        public void DeleteResourceGroup(string resourceGroupName)
+        {
+            _azure.ResourceGroups.DeleteByName(resourceGroupName);
+        }
+
+        // Create a virtual network.
+        public INetwork CreateVirtualNetwork(string vNetName, string resourceGroupName, Region location, string vNetAddress, string subnetAddress, string subnetName)
+        {
+            //Every virtual machine needs to be connected to a virtual network.
+            INetwork network = _azure.Networks.Define(vNetName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(resourceGroupName)
+                .WithAddressSpace(vNetAddress)
+                .WithSubnet(subnetName, subnetAddress)
+                .Create();
+
+            // network.Id = the network id (if you want to delete it by ID)
+
+            return network;
+        }
+
+        // Delete a virtual network by ID
+        public void DeleteVirtualNetwork(string networkId)
+        {
+            _azure.Networks.DeleteById(networkId);
+        }
+
+        // Delete a virtual network by ResourceGroup
+        public void DeleteVirtualNetwork(string resourceGroupName, string networkName)
+        {
+            _azure.Networks.DeleteByResourceGroup(resourceGroupName, networkName);
+        }
+
+        // Create a public IP
+        public IPublicIPAddress CreatePublicIp(string publicIPName, string resourceGroupName, Region location)
+        {
+            IPublicIPAddress publicIP = _azure.PublicIPAddresses.Define(publicIPName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(resourceGroupName)
+                .Create();
+
+            // publicIP.Id = the ID of the public IP (if you want to delete it by ID)
+
+            return publicIP;
+        }
+
+        // Delete a public IP by ID
+        public void DeletePublicIp(string publicIpId)
+        {
+            _azure.PublicIPAddresses.DeleteById(publicIpId);
+        }
+
+        // Delete a public IP by ResourceGroup
+        public void DeletePublicIp(string resourceGroupName, string publicIPName)
+        {
+            _azure.PublicIPAddresses.DeleteByResourceGroup(resourceGroupName, publicIPName);
+        }
+
+        // Create a Network Security Group
+        public INetworkSecurityGroup CreatNetworkSecurityGroup(string resourceGroupName, string nsgName, Region location)
+        {
+            INetworkSecurityGroup nsg = _azure.NetworkSecurityGroups.Define(nsgName)
+                .WithRegion(location)
+                .WithExistingResourceGroup(resourceGroupName)
+                .Create();
+
+            // nsg.Id = NetworkSecurityGroup ID (if you want to delete it by ID)
+
+            return nsg;
+        }
+
+        // Delete a Network Security Group by ID
+        public void DeleteNetworkSecurityGroup(string publicIpId)
+        {
+            _azure.NetworkSecurityGroups.DeleteById(publicIpId);
+        }
+
+        // Delete a Network Security Group by ResourceGroup
+        public void DeleteNetworkSecurityGroup(string resourceGroupName, string publicIPName)
+        {
+            _azure.NetworkSecurityGroups.DeleteByResourceGroup(resourceGroupName, publicIPName);
+        }
+
+        // Create a Network Interface
+        public INetworkInterface CreateNetworkInterface(string nicName, string resourceGroupName, Region location, INetwork network, string subnetName, IPublicIPAddress publicIPAddr, INetworkSecurityGroup nsg)
+        {
+            INetworkInterface nic = _azure.NetworkInterfaces.Define(nicName)
+                     .WithRegion(location)
+                     .WithExistingResourceGroup(resourceGroupName)
+                     .WithExistingPrimaryNetwork(network)
+                     .WithSubnet(subnetName)
+                     .WithPrimaryPrivateIPAddressDynamic()
+                     .WithExistingPrimaryPublicIPAddress(publicIPAddr)
+                     .WithExistingNetworkSecurityGroup(nsg)
+                     .Create();
+
+            return nic;
+        }
+
+        public void AllowInternetConnection(INetworkSecurityGroup nsg, int port, SecurityRuleProtocol protocol)
+        {
+            nsg.Update()
+                .DefineRule("Allow-RDP")
+                .AllowInbound()
+                .FromAnyAddress()
+                .FromAnyPort()
+                .ToAnyAddress()
+                .ToPort(port)
+                .WithProtocol(protocol)
+                .WithPriority(100)
+                .Attach()
+                .Apply();
+        }
+
+        // Create an WindowsServer Virtual Machine
+        public IVirtualMachine CreateVM (string vmName, string resourceGroupName, Region location, INetworkInterface nic, string adminUser, string adminPassword)
+        {
+            IVirtualMachine vm = _azure.VirtualMachines.Define(vmName)
+                    .WithRegion(location)
+                    .WithExistingResourceGroup(resourceGroupName)
+                    .WithExistingPrimaryNetworkInterface(nic)
+                    .WithLatestWindowsImage("MicrosoftWindowsServer", "WindowsServer", "2012-R2-Datacenter")
+                    .WithAdminUsername(adminUser)
+                    .WithAdminPassword(adminPassword)
+                    .WithComputerName(vmName)
+                    .WithSize(VirtualMachineSizeTypes.StandardDS2V2)
+                    .Create();
+           
+            return vm;
+        }
+
+        // Delete a Virtual Machine by ID
+        public void DeleteVM(string id, bool force = true)
+        {
+            _azure.VirtualMachines.DeleteById(id, force);
+        }
+
+        // Delete a Virtual Machine by ResourceGroup
+        public void DeleteVM(string resourceGroupName, string vmName, bool force = true)
+        {
+            _azure.VirtualMachines.DeleteByResourceGroup(resourceGroupName, vmName, force);
+        }
+    }
+
+
+    public class AzureVM
+    {
+        private readonly IAzure _azure;
+        private readonly string _vmName;
+
+        private readonly IVirtualMachine _VM;
+
+        public AzureVM(IAzure azure, string vmName)
+        {
+            this._azure = azure;
+            _vmName = vmName;
+
+            if (azure is null) 
+                return;
+
+            foreach (IVirtualMachine virtualMachine in this._azure.VirtualMachines.List())
+            {
+                if (virtualMachine.Name == _vmName)
+                {
+                    _VM = virtualMachine;
+                }
+            }
+        }
+
+        public PowerState GetState()
+        {
+            return _VM.PowerState;
+        }
+
+        public void Start()
+        {
+            _VM.Start();
+        }
+
+        public void Stop()
+        {
+            _VM.PowerOff();
+        }
+    }
+}
